ca4e5afedad3b155543bafb05b4f6a5aa165245a
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-09-19 23:32:04 +0200

    added weather-block

--- src/components/UI/Search/index.tsx
+++ src/components/UI/Search/index.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import styles from './style.module.scss';
 import iconUrl from '../../../assets/img/icons/search.svg';
 function Search() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
   return (
-    <div className={styles.search}>
-      <img src={iconUrl} alt="icon" />
+    <div
+      className={styles.search}
+      onClick={() => setIsOpen(!isOpen)}
+      onSubmit={(e: React.SyntheticEvent) => e.preventDefault()}
+    >
+      {isOpen ? (
+        <div className={styles.search__input}>
+          <img src={iconUrl} alt="icon" />
+          <input
+            name="search"
+            type="search"
+            placeholder="Search"
+            autoFocus
+            value={searchValue}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchValue(e.target.value)
+            }
+          />
+        </div>
+      ) : (
+        <img src={iconUrl} alt="icon" />
+      )}
     </div>
   );
 }
--- src/components/UI/Weather/index.tsx
+++ src/components/UI/Weather/index.tsx
@@ -1,16 +1,71 @@
+import { useState, useEffect } from 'react';
 import styles from './style.module.scss';
-import iconUrl from '../../../assets/img/icons/cloud.svg';
+import iconisLoading from '../../../assets/img/icons/animated_loader_gif_n6b5x0.gif';
+
+const KEY = '3fa1b2022990b86e709116d551f9ac84';
+const API__Url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=Warszawa&appid=${KEY}`;
+
+interface IWeather {
+  weather: { icon: string; main: string }[];
+  main: { temp: number };
+}
 
 function Weather() {
+  const [dataWeather, setDataWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+  let weatherIcon,
+    weatherStatus,
+    temperatura = null;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(API__Url, { method: 'GET' });
+        const data = await res.json();
+        setDataWeather(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+  if (error) {
+    return <h2> Error:{error}</h2>;
+  }
+  function getWeather(data: IWeather) {
+    weatherIcon = data.weather[0].icon;
+    weatherStatus = data.weather[0].main;
+    temperatura = Math.round(data.main.temp);
+  }
+
+  if (dataWeather) {
+    console.log(dataWeather);
+    getWeather(dataWeather);
+  }
+  console.log({ weatherIcon }, { weatherStatus }, { temperatura });
   return (
     <div className={styles.weather}>
-      <div className={styles.weather__status}></div>
-      <div className={styles.weather__icon}>
-        <img src={iconUrl} alt="icon-weather" />
-      </div>
-      <div className={styles.weather__temperature}>
-        15 <span>&#176;</span>C
-      </div>
+      {isLoading ? (
+        <div className={styles.isLoading}>
+          <img src={iconisLoading} alt="loading..." />
+        </div>
+      ) : (
+        <>
+          <div className={styles.weather__icon}>
+            <img
+              src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
+              alt={weatherStatus}
+            />
+          </div>
+          <div className={styles.weather__temperature}>
+            {temperatura} <span>&#176;</span>C
+          </div>
+        </>
+      )}
     </div>
   );
 }
